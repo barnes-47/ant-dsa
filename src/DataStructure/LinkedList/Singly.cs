8094ddc21092a4a9ca91c3c035766c9cd84c892d
Vedant Phougat <vedant.phougat@bdgloballogic.onmicrosoft.com>
2020-03-24 15:45:30 +0530

    source code + UTCs

---
+++ src/DataStructure/LinkedList/Singly.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ds.LinkedList
+{
+    public class Node
+    {
+        public long Data { get; }
+        public Node Next { get; set; }
+
+        public Node(long data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class Singly
+    {
+        #region Public Getters
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public ulong Length { get; private set; }
+        public bool IsNull => this == null;
+        public bool IsEmpty => Length == 0 && Head == null && Tail == null;
+        #endregion
+
+        #region Public Methods
+        public void AddFirst(long data)
+        {
+            var newNode = new Node(data);
+            if (IsEmpty)
+            {
+                Head = Tail = newNode;
+                ++Length;
+                return;
+            }
+
+            newNode.Next = Head;
+            Head = newNode;
+            ++Length;
+        }
+        
+        public void AddLast(long data)
+        {
+            var newNode = new Node(data);
+            if (IsEmpty)
+            {
+                Head = Tail = newNode;
+                ++Length;
+                return;
+            }
+
+            Tail.Next = newNode;
+            Tail = newNode;
+            ++Length;
+        }
+
+        public void AddAfter(Node existingNode, long data) => AddAfter(existingNode, new Node(data));
+        public void AddAfter(Node existingNode, Node newNode)
+        {
+            if (existingNode == null)
+                throw new ArgumentNullException(nameof(existingNode));
+            if (IsEmpty)
+            {
+                Head = existingNode;
+                Head.Next = Tail = newNode;
+                Length += 2;
+
+                return;
+            }
+            if (IsHead(existingNode))
+            {
+                AddAfterHead(existingNode);
+
+                return;
+            }
+            if (IsTail(existingNode))
+            {
+                AddAfterTail(existingNode);
+
+                return;
+            }
+
+            var current = Head.Next;
+            while(current != null)
+            {
+                if (current.Data == existingNode.Data)
+                {
+                    newNode.Next = existingNode.Next;
+                    existingNode.Next = newNode;
+                    ++Length;
+
+                    break;
+                }
+                current = current.Next;
+            }
+        }
+
+        public void Delete(long data)
+        {
+
+        }
+
+        public bool IsHead(long data) => IsHead(new Node(data));
+
+        public bool IsHead(Node node) => Head.Data == node.Data;
+
+        public bool IsTail(long data) => IsTail(new Node(data));
+
+        public bool IsTail(Node node) => Tail.Data == node.Data;
+
+        public bool Exists(long data) => Exists(new Node(data));
+
+        public bool Exists(Node node)
+        {
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+            if (IsEmpty)
+                return false;
+            if (IsHead(node))
+                return true;
+            if (IsTail(node))
+                return true;
+
+            var current = Head.Next;
+            while(current != null)
+            {
+                if (current.Data == node.Data)
+                    return true;
+                current = current.Next;
+            }
+
+            return false;
+        }
+        #endregion
+
+        #region Private Methods
+        private void AddAfterHead(long data) => AddAfterHead(new Node(data));
+
+        private void AddAfterHead(Node node)
+        {
+            node.Next = Head.Next;
+            Head = node;
+            ++Length;
+        }
+
+        private void AddAfterTail(long data) => AddAfterTail(new Node(data));
+
+        private void AddAfterTail(Node node)
+        {
+            Tail.Next = node;
+            Tail = node;
+            ++Length;
+        }
+        #endregion
+    }
+}
---
+++ test/DataStructure/LinkedList/SingleUnitTest.cs
@@ -0,0 +1,176 @@
+﻿using Ds.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ds.Test.LinkedList
+{
+    public class SingleUnitTest
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-111)]
+        [InlineData(0)]
+        [InlineData(5678)]
+        public void AddFirst_ThrowsNullReferenceException_WhenNull(long testData)
+        {
+            Singly singly = null;
+            var expectedEx = new NullReferenceException();
+
+            var actualEx = Assert.Throws<NullReferenceException>(() => singly.AddFirst(testData));
+
+            Assert.True(expectedEx.Message == actualEx.Message);
+            Assert.True(expectedEx.GetType().FullName == actualEx.GetType().FullName);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-111)]
+        [InlineData(0)]
+        [InlineData(5678)]
+        public void AddFirst_AddsOneNode_WhenEmpty(long testData)
+        {
+            var singly = new Singly();
+            var length = 1UL;
+            singly.AddFirst(testData);
+
+            Assert.NotNull(singly);
+            Assert.NotNull(singly.Head);
+            Assert.NotNull(singly.Tail);
+            Assert.False(singly.IsNull);
+            Assert.False(singly.IsEmpty);
+            Assert.True(singly.Head.Equals(singly.Tail));
+            Assert.True(singly.Head.Data == testData);
+            Assert.True(singly.Tail.Data == testData);
+            Assert.True(singly.Length == length);
+        }
+
+        [Theory]
+        [InlineData("1,2,-3,4,0,-5,6,7,8,9")]
+        [InlineData("3,8,1,5,-2,-9,7,6,0,4")]
+        [InlineData("12,42,57,99,87,21,56,93,36,54")]
+        public void AddFirst_AddsMultipleNodes_WhenEmpty(string testStr)
+        {
+            var singly = new Singly();
+            var mockData = Array.ConvertAll(testStr.Split(','), long.Parse);
+            foreach(var d in mockData)
+            {
+                singly.AddFirst(d);
+            }
+
+            Assert.Null(singly.Tail.Next);
+            Assert.NotNull(singly.Head);
+            Assert.NotNull(singly.Tail);
+            Assert.NotNull(singly.Head.Next);
+            Assert.False(singly.Head.Equals(singly.Tail));
+            Assert.False(singly.IsNull);
+            Assert.False(singly.IsEmpty);
+            Assert.True(singly.Length == (ulong)mockData.Length);
+
+            Array.Reverse(mockData);
+            var current = singly.Head;
+            foreach (var d in mockData)
+            {
+                Assert.True(current.Data == d);
+                current = current.Next;
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-111)]
+        [InlineData(0)]
+        [InlineData(5678)]
+        public void AddLast_ThrowsNullReferenceException_WhenNull(long testData)
+        {
+            Singly singly = null;
+            var expectedEx = new NullReferenceException();
+
+            var actualEx = Assert.Throws<NullReferenceException>(() => singly.AddLast(testData));
+
+            Assert.True(expectedEx.Message == actualEx.Message);
+            Assert.True(expectedEx.GetType().FullName == actualEx.GetType().FullName);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-111)]
+        [InlineData(0)]
+        [InlineData(5678)]
+        public void AddLast_AddsOneNode_WhenEmpty(long testData)
+        {
+            var singly = new Singly();
+            var length = 1UL;
+            singly.AddLast(testData);
+
+            Assert.NotNull(singly);
+            Assert.NotNull(singly.Head);
+            Assert.NotNull(singly.Tail);
+            Assert.False(singly.IsNull);
+            Assert.False(singly.IsEmpty);
+            Assert.True(singly.Head.Equals(singly.Tail));
+            Assert.True(singly.Head.Data == testData);
+            Assert.True(singly.Tail.Data == testData);
+            Assert.True(singly.Length == length);
+        }
+
+        [Theory]
+        [InlineData("1,2,-3,4,0,-5,6,7,8,9")]
+        [InlineData("3,8,1,5,-2,-9,7,6,0,4")]
+        [InlineData("12,42,57,99,87,21,56,93,36,54")]
+        public void AddLast_AddsMultipleNodes_WhenEmpty(string testStr)
+        {
+            var singly = new Singly();
+            var mockData = Array.ConvertAll(testStr.Split(','), long.Parse);
+            foreach (var d in mockData)
+            {
+                singly.AddLast(d);
+            }
+
+            Assert.Null(singly.Tail.Next);
+            Assert.NotNull(singly.Head);
+            Assert.NotNull(singly.Tail);
+            Assert.NotNull(singly.Head.Next);
+            Assert.False(singly.Head.Equals(singly.Tail));
+            Assert.False(singly.IsNull);
+            Assert.False(singly.IsEmpty);
+            Assert.True(singly.Length == (ulong)mockData.Length);
+
+            var current = singly.Head;
+            foreach (var d in mockData)
+            {
+                Assert.True(current.Data == d);
+                current = current.Next;
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(-111)]
+        [InlineData(0)]
+        [InlineData(5678)]
+        public void AddAfter_ThrowsArgumentNullException_WhenNodeIsNull(long testData)
+        {
+            var singly = new Singly();
+            var expectedEx = new ArgumentNullException("existingNode");
+            Node testNode = null;
+
+            var actualEx = Assert.Throws<ArgumentNullException>(() => singly.AddAfter(testNode, testData));
+
+            Assert.True(expectedEx.ParamName == actualEx.ParamName);
+            Assert.True(expectedEx.Message == actualEx.Message);
+            Assert.True(expectedEx.GetType().FullName == actualEx.GetType().FullName);
+        }
+
+        //[Theory]
+        //[InlineData(1)]
+        //[InlineData(-111)]
+        //[InlineData(0)]
+        //[InlineData(5678)]
+        //public void AddAfter_AddsTwoNodes_WhenEmpty(long testData)
+        //{
+        //    var singly = new Singly();
+        //}
+    }
+}
