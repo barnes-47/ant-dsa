ff50aa5213ddf0cb4ba51e0a33ab04ec96c9b2be
Vedant Phougat <vedant.phougat@bdgloballogic.onmicrosoft.com>
2020-04-24 00:07:38 +0530

    UTCs for Operations.OnStack.

---
+++ src/data-structure/Operation/OnStack.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Ds.Operation
+{
+    public class OnStack<T>
+    {
+        #region Public Properties
+        public Generic.Stack<T> Stack { get; private set; }
+        #endregion
+
+        #region Public Ctors
+        public OnStack()
+        {
+            Stack = new Generic.Stack<T>();
+        }
+
+        public OnStack(int capacity)
+        {
+            Stack = new Generic.Stack<T>(capacity);
+        }
+
+        public OnStack(IEnumerable<T> collection)
+        {
+            Stack = new Generic.Stack<T>(collection);
+        }
+        #endregion
+
+        /// <summary>
+        /// Inserts the item at the bottom of the stack using recursion.
+        /// </summary>
+        public void InsertAtBottomUsingRecursion(T item)
+        {
+            if (Stack.IsEmpty)
+            {
+                Stack.Push(item);
+                return;
+            }
+
+            var top = Stack.Pop();
+            InsertAtBottomUsingRecursion(item);
+            Stack.Push(top);
+        }
+
+        /// <summary>
+        /// Reverses the elements of the stack using recursion.
+        /// </summary>
+        public void ReverseUsingRecursion()
+        {
+            if (Stack.IsEmpty)
+                return;
+
+            var top = Stack.Pop();
+            ReverseUsingRecursion();
+            InsertAtBottomUsingRecursion(top);
+        }
+
+        /// <summary>
+        /// Reverses a Stack using extra stack.
+        /// </summary>
+        public void ReverseUsingExtraSpace()
+        {
+            var stack = new Generic.Stack<T>(Stack.Count);
+            foreach (var item in Stack)
+            {
+                stack.Push(item);
+            }
+
+            Stack = stack;
+        }
+    }
+}
---
+++ test/data-structure/Operation/OnStackUtc.cs
@@ -0,0 +1,109 @@
+﻿using DsGeneric = Ds.Generic;
+using Ds.Operation;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Ds.Test.Operation
+{
+    public class OnStackUtc
+    {
+        #region Private Assert Methods
+        private void AssertTrueZeroAndActualStackCount<T>(DsGeneric.Stack<T> actual)
+            => Assert.True(0 == actual.Count);
+        private void AssertTrueCountOfExpectedStackAndActualStack<T>(DsGeneric.Stack<T> expected, DsGeneric.Stack<T> actual)
+            => Assert.True(expected.Count == actual.Count);
+        private void AssertTrueElementsOfExpectedStackAndActualStack<T>(DsGeneric.Stack<T> expected, DsGeneric.Stack<T> actual)
+        {
+            var comparer = EqualityComparer<T>.Default;
+            while(expected.Count > 0 && actual.Count > 0)
+            {
+                Assert.True(expected.Count == actual.Count);
+                Assert.True(comparer.Equals(expected.Pop(), actual.Pop()));
+            }
+        }
+        private void AssertTrueExpectedOnStackAndActualOnStack<T>(OnStack<T> expected, OnStack<T> actual)
+        {
+            AssertTrueElementsOfExpectedStackAndActualStack<T>(expected.Stack, actual.Stack);
+            AssertTrueCountOfExpectedStackAndActualStack<T>(expected.Stack, actual.Stack);
+        }
+        #endregion
+
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("ab", "ba")]
+        [InlineData("abc", "cba")]
+        [InlineData("abcd", "dcba")]
+        [InlineData("abcde", "edcba")]
+        public void InsertAtBottomUsingRecursion_InsertsItemAtTheBottomOfTheStack_ForCharStack(
+            string actualStr
+            , string expectedStr)
+        {
+            var actualOnStack = new OnStack<char>();
+            var expectedOnStack = new OnStack<char>(expectedStr.ToCharArray());
+
+            for (var i = 0; i < actualStr.Length; i++)
+            {
+                actualOnStack.InsertAtBottomUsingRecursion(actualStr[i]);
+            }
+
+            AssertTrueElementsOfExpectedStackAndActualStack<char>(expectedOnStack.Stack, actualOnStack.Stack);
+            AssertTrueZeroAndActualStackCount(actualOnStack.Stack);
+        }
+
+        [Theory]
+        [InlineData("aa", "aa")]
+        [InlineData("aa,bb", "bb,aa")]
+        [InlineData("aa,bb,cc", "cc,bb,aa")]
+        [InlineData("aa,bb,cc,dd", "dd,cc,bb,aa")]
+        public void InsertAtBottomUsingRecursion_InsertsItemAtTheBottomOfTheStack_ForStringStack(
+            string actualStr
+            , string expectedStr)
+        {
+            var actualStrArray = actualStr.Split(',');
+            var actualOnStack = new OnStack<string>();
+            var expectedOnStack = new OnStack<string>(expectedStr.Split(','));
+
+            for (var i = 0; i < actualStrArray.Length; i++)
+            {
+                actualOnStack.InsertAtBottomUsingRecursion(actualStrArray[i]);
+            }
+
+            AssertTrueElementsOfExpectedStackAndActualStack<string>(expectedOnStack.Stack, actualOnStack.Stack);
+            AssertTrueZeroAndActualStackCount(actualOnStack.Stack);
+        }
+
+        [Theory]
+        [InlineData("aa", "aa")]
+        [InlineData("aa,bb", "bb,aa")]
+        [InlineData("aa,bb,cc", "cc,bb,aa")]
+        [InlineData("aa,bb,cc,dd", "dd,cc,bb,aa")]
+        public void ReverseUsingRecursion_ReversesStackUsingRecursion(
+            string actualStr
+            , string expectedStr)
+        {
+            var actualOnStack = new OnStack<string>(actualStr.Split(','));
+            var expectedOnStack = new OnStack<string>(expectedStr.Split(','));
+
+            actualOnStack.ReverseUsingRecursion();
+
+            AssertTrueExpectedOnStackAndActualOnStack<string>(expectedOnStack, actualOnStack);
+        }
+
+        [Theory]
+        [InlineData("aa", "aa")]
+        [InlineData("aa,bb", "bb,aa")]
+        [InlineData("aa,bb,cc", "cc,bb,aa")]
+        [InlineData("aa,bb,cc,dd", "dd,cc,bb,aa")]
+        public void ReverseUsingExtraSpace_ReversesStackUsingExtraSpace(
+            string actualStr
+            , string expectedStr)
+        {
+            var actualOnStack = new OnStack<string>(actualStr.Split(','));
+            var expectedOnStack = new OnStack<string>(expectedStr.Split(','));
+
+            actualOnStack.ReverseUsingExtraSpace();
+
+            AssertTrueExpectedOnStackAndActualOnStack<string>(expectedOnStack, actualOnStack);
+        }
+    }
+}
