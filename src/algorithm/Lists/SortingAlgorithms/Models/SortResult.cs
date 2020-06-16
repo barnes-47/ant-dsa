54e23c531edaabee84342929b2357cd532706c00
Vedant Phougat <vedant@phougat.com>
2020-06-16 15:45:21 +0530

    Added SortResult model.

---
+++ src/algorithm/Lists/SortingAlgorithms/Models/SortResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Algo.Lists.SortingAlgorithms.Models
+{
+    public class SortResult
+    {
+        public IList<int> Elements { get; set; } = new List<int>();
+        public long Ticks { get; set; }
+    }
+}
