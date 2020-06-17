ab52096c2cf97118c930bdd94e787870dd095a03
Vedant Phougat <vedant@phougat.com>
2020-06-17 16:05:53 +0530

    Added problems.

--- src/practice/Problem.cs
+++ src/practice/Problem.cs
@@ -7,14 +7,96 @@ namespace Practice
     {
         #region Public Methods
         /// <summary>
-        /// Finds the two numbers in the list that adds to the number passed.
+        /// This problem was recently asked by Google.
+        /// 
+        /// Given a list of numbers and a number k,
+        /// return whether any two numbers from the list add up to k.
+        /// 
+        /// For example, given[10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+        /// 
+        /// Bonus: Can you do this in one pass?
         /// </summary>
         /// <param name="num">The number.</param>
-        public bool FindTwoNumbersInTheArrayThatAddsTo(IList<int> unorderedList, int num)
+        public bool GetTwoNumbersInTheArrayThatAddsTo(IList<int> unorderedList, int num)
             => FindTwoIntsInTheListThatAddsTo(unorderedList, num).Any();
+
+        /// <summary>
+        /// This problem was asked by Uber.
+        /// 
+        /// Given an array of integers, return a new array such that
+        /// each element at index i of the new array is the product of
+        /// all the numbers in the original array except the one at i.
+        /// 
+        /// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+        /// If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+        /// 
+        /// Follow-up: what if you can't use division?
+        /// </summary>
+        /// <returns></returns>
+        public IList<int> GetTheMultiplicationArray(IList<int> unorderedList)
+        {
+            #region First Idea
+            var product = 1;
+            var count = 0;
+            while (count < unorderedList.Count)
+            {
+                product *= unorderedList[count++];
+            }
+
+            var productList = new List<int>();
+            count = 0;
+            while (count < unorderedList.Count)
+            {
+                productList.Add(product / unorderedList[count++]);
+            }
+
+            return productList;
+            #endregion
+
+            #region Follow-up
+
+            #endregion
+        }
         #endregion
 
-        #region Private Methods
+        #region Private Methods        
+        /// <summary>Gets the sum of n terms in an arithmetic progression.</summary>
+        /// <param name="first">The first element.</param>
+        /// <param name="diff">The difference between two consecutive elements.</param>
+        /// <param name="n">The number of elements.</param>
+        /// <returns></returns>
+        private int GetSumOfNTermsInArithmeticProgression(int first, int diff, int n)
+        {
+            if ((n & 1) == 1)   //When n is ODD.
+                return n * (((2 * first) + ((n - 1) * diff)) / 2);
+
+            return n / 2 * ((2 * first) + ((n - 1) * diff));
+        }
+
+        /// <summary>Gets the factorial of the number passed.</summary>
+        /// <param name="n">The number.</param>
+        /// <returns></returns>
+        public int GetFactorial(int n)
+        {
+            var product = 1;
+            while (n > 1)
+            {
+                product *= n--;
+            }
+
+            return product;
+        }
+
+        /// <summary>Gets the factorial of the number passed using recursion.</summary>
+        /// <param name="n">The number.</param>
+        /// <returns></returns>
+        public int GetFactorial_Recursion(int n)
+        {
+            if (n < 2)
+                return 1;
+            return n * GetFactorial_Recursion(n - 1);
+        }
+
         private IList<int> FindTwoIntsInTheListThatAddsTo(IList<int> unordered, int num)
         {
             //Sort the list.
--- test/practice/ProblemTests.cs
+++ test/practice/ProblemTests.cs
@@ -12,9 +12,49 @@ namespace Practice.Test
             var unorderedList = new List<int> { 10, 2, 11, 12, 7 };
             var problem = new Problem();
 
-            var actual = problem.FindTwoNumbersInTheArrayThatAddsTo(unorderedList, num);
+            var actual = problem.GetTwoNumbersInTheArrayThatAddsTo(unorderedList, num);
 
             Assert.True(actual);
+        }
+
+        [Fact]
+        public void FindTwoNumbersInTheArrayThatAddsTo_Returns_False()
+        {
+            var num = 121;
+            var unorderedList = new List<int> { 10, 2, 11, 12, 7 };
+            var problem = new Problem();
+
+            var actual = problem.GetTwoNumbersInTheArrayThatAddsTo(unorderedList, num);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void GetTheMultiplicationArray_ReturnsTheMultiplicationArray()
+        {
+            var unorderedList = new List<int> { 1, 2, 3, 4, 5 };
+            var expected = new List<int> { 120, 60, 40, 30, 24 };
+            var problem = new Problem();
+
+            var actual = problem.GetTheMultiplicationArray(unorderedList);
+
+            for (var i = 0; i < actual.Count; ++i)
+            {
+                Assert.True(expected[i] == actual[i]);
+            }
+        }
+
+        [Fact]
+        public void Test()
+        {
+            //var unorderedList = new List<int> { 1, 2, 3, 4, 5 };
+            //var expected = new List<int> { 120, 60, 40, 30, 24 };
+
+            //var d = 120 >> 1;
+            //var c = 120 >> 2;
+            //var e = 120 >> 3;
+            //var f = 120 >> 4;
+            //var g = 120 >> 5;
         }
     }
 }
