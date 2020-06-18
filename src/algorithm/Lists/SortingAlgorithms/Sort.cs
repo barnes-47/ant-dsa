fd77453d297cdabe5a75e903610d1edf0b80ad30
Vedant Phougat <vedant@phougat.com>
2020-06-18 21:12:49 +0530

    Changes

--- src/algorithm/Lists/SortingAlgorithms/Sort.cs
+++ src/algorithm/Lists/SortingAlgorithms/Sort.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections.Generic;
-
-namespace Algo.Lists.SortingAlgorithms
+﻿namespace Algo.Lists.SortingAlgorithms
 {
+    using Algo.Lists.SortingAlgorithms.Models;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
     public static class Sort
     {
         /// <summary>
+        /// Bubble sort algorithm.
         /// Time complexity:    O(n*n)
         /// Space complexity:   O(n)
-        /// Perfoms bubble sorting on a collection of integers.
         /// 
         /// The only significant advantage that bubble sort has over most other algorithms,
         /// is that the ability to detect that the list is already sorted is built into the
@@ -21,13 +23,17 @@ namespace Algo.Lists.SortingAlgorithms
         /// It should be avoided in the case of large collections.
         /// </summary>
         /// <param name="elements">The elements.</param>
-        public static void BubbleSort(this IList<int> elements)
+        public static SortResult BubbleSort(this IList<int> elements)
         {
+            var result = new SortResult();
+            var sw = Stopwatch.StartNew();
+
+            //Bubble Sort Algorithm
+            int i, j;
             var swapped = true;
             while (swapped)
             {
-                var i = -1;
-                var j = 0;
+                i = -1; j = 0;
                 swapped = false;
                 while ((++i < (elements.Count - 1)) && (++j < elements.Count))
                 {
@@ -41,30 +47,122 @@ namespace Algo.Lists.SortingAlgorithms
                     }
                 }
             }
+
+            sw.Stop();
+            result.Ticks = sw.ElapsedTicks;
+            result.Elements = elements;
+
+            return result;
         }
 
-        public static void InsertionSort(this IList<int> elements)
+        /// <summary>
+        /// Insertion sort algorithm.
+        /// Time complexity:    O(n*n)
+        /// Space complexity:   O(1)
+        /// 
+        /// It divides(logically) the list into 2 parts - sorted and unsorted.
+        /// The first element of the list is considered the sorted and
+        /// remaining list is considered unsorted.
+        /// At a time it picks one element from unsorted list and puts it in it's place in the sorted list.
+        /// 
+        /// It is much less efficient on large lists than more advanced algorithms,
+        /// such as QuickSort, HeadSort and MergeSort.
+        /// </summary>
+        /// <param name="elements">The elements.</param>
+        public static SortResult InsertionSort(this IList<int> elements)
         {
-            var sortedLastIndex = 0;
-            var unsortedFirstIndex = 0;
-            var unsortedFirst = elements[1];
-            while (++unsortedFirstIndex < elements.Count)
-            {
-                sortedLastIndex = unsortedFirstIndex - 1;
-                unsortedFirst = elements[unsortedFirstIndex];
+            var result = new SortResult();
+            var sw = Stopwatch.StartNew();
 
-                if (unsortedFirst < elements[sortedLastIndex]) //when unsorted first is less than sorted last.
+            //Insertion Sort Algorithm
+            var sortedIndex = 0;
+            var unsortedIndex = 0;
+            var firstUnsortedElement = elements[1];
+            while (++unsortedIndex < elements.Count)
+            {
+                sortedIndex = unsortedIndex - 1;
+                firstUnsortedElement = elements[unsortedIndex];
+
+                if (firstUnsortedElement < elements[sortedIndex]) //when unsorted first is less than sorted last.
                 {
-                    while ((0 <= sortedLastIndex) && (unsortedFirst < elements[sortedLastIndex]))
+                    while ((0 <= sortedIndex) && (firstUnsortedElement < elements[sortedIndex]))
                     {
                         //swap the elements
-                        elements[sortedLastIndex] = elements[sortedLastIndex] + elements[sortedLastIndex + 1];
-                        elements[sortedLastIndex + 1] = elements[sortedLastIndex] - elements[sortedLastIndex + 1];
-                        elements[sortedLastIndex] = elements[sortedLastIndex] - elements[sortedLastIndex + 1];
-                        --sortedLastIndex;
+                        elements[sortedIndex] = elements[sortedIndex] + elements[sortedIndex + 1];
+                        elements[sortedIndex + 1] = elements[sortedIndex] - elements[sortedIndex + 1];
+                        elements[sortedIndex] = elements[sortedIndex] - elements[sortedIndex + 1];
+                        --sortedIndex;
                     }
                 }
             }
+
+            sw.Stop();
+            result.Ticks = sw.ElapsedTicks;
+            result.Elements = elements;
+
+            return result;
         }
+
+        public static SortResult MergeSort(this IList<int> elements)
+        {
+            var result = new SortResult();
+            var sw = Stopwatch.StartNew();
+
+            InternalSort(elements, 0, elements.Count - 1);
+
+            sw.Stop();
+            result.Ticks = sw.ElapsedTicks;
+            result.Elements = elements;
+
+            return result;
+        }
+
+        #region Private Methods
+        private static void InternalListCopy(IList<int> actual, IList<int> mirror)
+        {
+            var i = -1;
+            while (++i < actual.Count)
+                mirror.Add(actual[i]);
+        }
+        private static void InternalMerge(IList<int> elements, int begin, int middle, int end)
+        {
+            //Create 2 lists
+            var listX = new List<int>();
+            var listY = new List<int>();
+            for (var i = begin; i <= middle; ++i)
+                listX.Add(elements[i]);
+            for (var i = middle + 1; i <= end; ++i)
+                listY.Add(elements[i]);
+
+            //Loop through both lists to compare and replace the elements
+            var iX = 0;
+            var iY = 0;
+            var iElement = begin;
+            while (iX < listX.Count && iY < listY.Count)
+            {
+                if (listX[iX] <= listY[iY])
+                    elements[iElement] = listX[iX++];
+                else
+                    elements[iElement] = listY[iY++];
+                ++iElement;
+            }
+
+            //Copy remaining elements
+            while (iX < listX.Count)
+                elements[iElement++] = listX[iX++];
+            while (iY < listY.Count)
+                elements[iElement++] = listY[iY++];
+        }
+        private static void InternalSort(IList<int> elements, int begin, int end)
+        {
+            if (end <= begin)
+                return;
+
+            var middle = (end + begin) / 2;
+            InternalSort(elements, begin, middle);
+            InternalSort(elements, middle + 1, end);
+            InternalMerge(elements, begin, middle, end);
+        }
+        #endregion
     }
 }
--- test/algorithms/Algo.Test/Lists/SortTests.cs
+++ test/algorithms/Algo.Test/Lists/SortTests.cs
@@ -1,11 +1,11 @@
-﻿using Algo.Lists.DataProvider;
-using Algo.Lists.SortingAlgorithms;
-using System;
-using System.Collections.Generic;
-using Xunit;
-
-namespace Algo.Test.Lists
+﻿namespace Algo.Test.Lists
 {
+    using Algo.Lists.SortingAlgorithms;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
     public class SortTests
     {
         [Theory]
@@ -14,19 +14,18 @@ namespace Algo.Test.Lists
         [InlineData("3 4 5 2 1")]
         [InlineData("2 3 4 5 1")]
         [InlineData("1 2 3 4 5")]
-        public void BubbleSort_SortsTheElementaryUnsortedList(
+        public void BubbleSort_SortsTheUnsortedList(
             string str)
         {
-            var ints = Array.ConvertAll(str.Split(' '), int.Parse);
-            var elements = new List<int>(ints);
+            var actual = Array.ConvertAll(str.Split(' '), int.Parse).ToList();
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
 
-            elements.BubbleSort();
+            actual.BubbleSort();
 
-            Assert.True(1 == elements[0]);
-            Assert.True(2 == elements[1]);
-            Assert.True(3 == elements[2]);
-            Assert.True(4 == elements[3]);
-            Assert.True(5 == elements[4]);
+            for (var i = 0; i < expected.Count; ++i)
+            {
+                Assert.True(expected[i] == actual[i]);
+            }
         }
 
         [Theory]
@@ -35,19 +34,37 @@ namespace Algo.Test.Lists
         [InlineData("3 4 5 2 1")]
         [InlineData("2 3 4 5 1")]
         [InlineData("1 2 3 4 5")]
-        public void InsertionSort_SortsTheElementaryUnsortedList(
+        public void InsertionSort_SortsTheUnsortedList(
             string str)
         {
-            var ints = Array.ConvertAll(str.Split(' '), int.Parse);
-            var elements = new List<int>(ints);
+            var actual = Array.ConvertAll(str.Split(' '), int.Parse).ToList();
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
 
-            elements.InsertionSort();
+            actual.InsertionSort();
 
-            Assert.True(1 == elements[0]);
-            Assert.True(2 == elements[1]);
-            Assert.True(3 == elements[2]);
-            Assert.True(4 == elements[3]);
-            Assert.True(5 == elements[4]);
+            for (var i = 0; i < expected.Count; ++i)
+            {
+                Assert.True(expected[i] == actual[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData("5 4 3 2 1")]
+        [InlineData("4 5 3 2 1")]
+        [InlineData("3 4 5 2 1")]
+        [InlineData("2 3 4 5 1")]
+        [InlineData("1 2 3 4 5")]
+        public void MergeSort_SortsAnUnsortedList(string str)
+        {
+            var actual = Array.ConvertAll(str.Split(' '), int.Parse).ToList();
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
+
+            actual.MergeSort();
+
+            for (var i = 0; i < expected.Count; ++i)
+            {
+                Assert.True(expected[i] == actual[i]);
+            }
         }
     }
 }
